368665ace3a85cab5c7639c13a276e0e02746c4f
TehCheat <thecheatuc@gmail.com>
2021-07-27 10:58:00 -0400

    updated for Qvin HUD. rebuilt dll

--- GameStatExporter/GameStatExporter/Settings.cs
+++ GameStatExporter/GameStatExporter/Settings.cs
@@ -1,23 +1,26 @@
 ﻿using System.Windows.Forms;
-using SharpDX;
-using PoeHUD.Plugins;
-using PoeHUD.Hud.Settings;
+using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
+using ExileCore.Shared.Nodes;
 
 namespace GameStatExporter
 {
 	//All properties and public fields of this class will be saved to file
-	public class Settings : SettingsBase
+	public class Settings : ISettings
 	{
 		public Settings()
 		{
-			Enable = true;
-		}
+            Enable = new ToggleNode(false);
+        }
 
-	    [Menu("Use commentary instead Description attribute")]
-	    public ToggleNode UseComments { get; set; } = true;
+        [Menu("Enable")]
+        public ToggleNode Enable { get; set; }
+
+        [Menu("Use commentary instead Description attribute")]
+	    public ToggleNode UseComments { get; set; } = new ToggleNode(true);
 
 	    [Menu("Save path")]
-        public TextNode SavePath { get; set; } = new TextNode();
+        public TextNode SavePath { get; set; } = new TextNode("");
 
 	    [Menu("Export!")]
 	    public ButtonNode ExportButtonNode { get; set; } = new ButtonNode();
